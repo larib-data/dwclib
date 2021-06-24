9c03c3117d9150d9f34d418c9cd106e16e88a706
Jona Joachim <jona@joachim.cc>
2021-06-24 11:31:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='dwclib',
-    version='0.20210624.01',
+    version='0.20210624.2',
     license='ISC',
     description='Python wrapper to DataWarehouse Connect.',
     long_description='Python wrapper to DataWarehouse Connect.',
