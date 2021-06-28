92d063be2d1ca4af7edcf9692eb67966c544d68d
Jona Joachim <jona@joachim.cc>
2021-06-28 22:32:34 +0200

    Fix datetime index

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='dwclib',
-    version='0.20210628.3',
+    version='0.20210628.4',
     license='ISC',
     description='Python wrapper to DataWarehouse Connect.',
     long_description='Python wrapper to DataWarehouse Connect.',
