d50df0d725db4299311badf60325d6d5c91a174f
Jona Joachim <jona@joachim.cc>
2022-05-05 21:03:15 +0200

    Move from flit to poetry and add support for linting with nox

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,60 @@
+import tempfile
+from typing import Any
+
 import nox
+from nox.sessions import Session
 
 locations = ["dwclib"]
 python_versions=['3.8']
 
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    """Install packages constrained by Poetry's lock file.
+    This function is a wrapper for nox.sessions.Session.install. It
+    invokes pip to install packages inside of the session's virtualenv.
+    Additionally, pip is passed a constraints file generated from
+    Poetry's lock file, to ensure that the packages are pinned to the
+    versions specified in poetry.lock. This allows you to manage the
+    packages as Poetry development dependencies.
+    Arguments:
+        session: The Session object.
+        args: Command-line arguments for pip.
+        kwargs: Additional keyword arguments for Session.install.
+    """
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
 @nox.session(python=python_versions)
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(session,
         "flake8",
         "flake8-bandit",
     )
     session.run("flake8", *args)
 
-@nox.session(python=python_versions)
-def safety(session):
-    session.install("flit","safety")
-    session.run("flit","build")
-    session.run("safety", "check", "--full-report")
+@nox.session(python="3.8")
+def safety(session: Session) -> None:
+    """Scan dependencies for insecure packages."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
