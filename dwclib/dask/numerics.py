70d868ac5380239144242ec28ca6e293307697a6
Jona Joachim <jona@joachim.cc>
2022-06-06 13:29:21 +0200

    Remove dask join engine

--- dwclib/dask/numerics.py
+++ dwclib/dask/numerics.py
@@ -3,7 +3,6 @@ from typing import List, Optional
 
 from dwclib.common.db import dwcuri
 
-from .numerics_dask import read_numerics as read_numerics_dask
 from .numerics_sql import read_numerics as read_numerics_sql
 
 one_hour = timedelta(hours=1)
@@ -15,30 +14,17 @@ def read_numerics(
     dtend,
     labels: Optional[List[str]] = None,
     interval=one_hour,
-    joinengine='sql',
     uri=None,
 ):
     if not uri:
         uri = dwcuri
     if labels is None:
         labels = []
-    if joinengine == 'sql':
-        return read_numerics_sql(
-            patientids,
-            dtbegin,
-            dtend,
-            uri,
-            labels,
-            interval,
-        )
-    elif joinengine == 'dask':
-        return read_numerics_dask(
-            patientids,
-            dtbegin,
-            dtend,
-            uri,
-            labels,
-            interval,
-        )
-    else:
-        return None
+    return read_numerics_sql(
+        patientids,
+        dtbegin,
+        dtend,
+        uri,
+        labels,
+        interval,
+    )
