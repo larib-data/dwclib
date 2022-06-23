74eacede929645741cfda1861257e80d71ab5614
Jona Joachim <jona@joachim.cc>
2022-06-23 14:44:48 +0200

    Add import shortcuts

--- dwclib/__init__.py
+++ dwclib/__init__.py
@@ -1,9 +1,19 @@
 """Python wrapper to DataWarehouse Connect."""
 
+from dwclib.waves import read_waves
+from dwclib.numerics import read_numerics
+from dwclib.patients import read_patients, read_patient
+
+__all__ = ['read_numerics']
+
 __all__ = [
     'common',
     'numerics',
     'waves',
     'patients',
     'dask',
+    'read_waves',
+    'read_numerics',
+    'read_patients',
+    'read_patient',
 ]
---
+++ dwclib/dask/__init__.py
@@ -0,0 +1,5 @@
+from .generic import read_data
+from .numerics import read_numerics
+from .waves import read_waves
+
+__all__ = ['read_numerics', 'read_waves', 'read_data']
