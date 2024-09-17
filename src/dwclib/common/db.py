a9b6a40d43413007e695c185f42f5457b0b86eef
Jona JOACHIM <jaj@star.jaj.hs.letemple.org>
2024-09-17 22:32:32 +0200

    Move from pyodbc to pymssql. Bump version

--- src/dwclib/common/db.py
+++ src/dwclib/common/db.py
@@ -1,50 +1,38 @@
-from configparser import ConfigParser
+import configparser
 from pathlib import Path
 from warnings import warn
-
 from platformdirs import user_config_dir
-
-try:
-    import pyodbc
-
-    pyodbc.pooling = False
-    odbcdriver = pyodbc.drivers()[0]
-except (ImportError, IndexError):
-    odbcdriver = None
-
 from sqlalchemy.engine import URL
 
 config_dir = Path(user_config_dir(appname="larib-data", appauthor="larib-data"))
 configfile = config_dir / "config.ini"
-
+dwcuri = None
+pguri = None
 
 if configfile.exists():
-    config = ConfigParser()
+    config = configparser.ConfigParser()
     config.read(configfile)
-    dwcdriver = config.get("dwc", "driver", fallback=odbcdriver)
-    if dwcdriver is None:
-        warn("No ODBC driver found for DWC", RuntimeWarning)
 
-    dwcuri = URL.create(
-        "mssql+pyodbc",
-        username=config.get("dwc", "user"),
-        password=config.get("dwc", "pass"),
-        host=config.get("dwc", "host"),
-        database=config.get("dwc", "dbname"),
-        query={
-            "driver": dwcdriver,
-            "Encrypt": "no",
-        },
-    )
+    try:
+        dwcuri = URL.create(
+            "mssql+pymssql",
+            username=config.get("dwc", "user"),
+            password=config.get("dwc", "pass"),
+            host=config.get("dwc", "host"),
+            database=config.get("dwc", "dbname"),
+        )
+    except configparser.NoOptionError:
+        warn(f"No valid DWC configuration in {configfile}.", RuntimeWarning)
 
-    pguri = URL.create(
-        "postgresql",
-        username=config.get("dwcmeta", "user"),
-        password=config.get("dwcmeta", "pass"),
-        host=config.get("dwcmeta", "host"),
-        database=config.get("dwcmeta", "dbname"),
-    )
+    try:
+        pguri = URL.create(
+            "postgresql",
+            username=config.get("dwcmeta", "user"),
+            password=config.get("dwcmeta", "pass"),
+            host=config.get("dwcmeta", "host"),
+            database=config.get("dwcmeta", "dbname"),
+        )
+    except configparser.NoOptionError:
+        warn(f"No valid DWCmeta configuration in {configfile}.", RuntimeWarning)
 else:
     warn(f"Configuration file {configfile} does not exist.", RuntimeWarning)
-    dwcuri = None
-    pguri = None
